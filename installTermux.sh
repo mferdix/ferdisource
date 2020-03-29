50e5605c4c8d286850a0b0b592b001bb2be4d197
mferdix <62669143+mferdix@users.noreply.github.com>
2020-03-30 02:01:24 +0700

    Create installTermux.sh

---
+++ installTermux.sh
@@ -0,0 +1,32 @@
+echo "========================"
+echo "   Installing Python3   "
+echo "========================"
+
+  pkg install python3
+
+echo "============================"
+echo "   Installing PIP Python3   "
+echo "============================"
+
+  pkg install python3-pip
+
+echo "==================="
+echo "   Upgrading PIP   "
+echo "==================="
+ 
+  pkg install --upgrade pip
+
+echo "==============================="
+echo "   Installing Python Library   "
+echo "==============================="
+
+  pip3 install pandas
+  pip3 install lxml
+  pip3 install bs4
+  pip3 install requests
+
+echo "================"
+echo "   Game Start   "
+echo "================"
+
+python3 index.py
