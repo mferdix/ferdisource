2dcb5182737fddc5c0c684b8a50226b92662aed8
mferdix <62669143+mferdix@users.noreply.github.com>
2020-04-01 01:09:33 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import requests
+import time
         
 print(" ")
 print("----------------------------------------------------------------------")
@@ -22,12 +23,22 @@ while(ulang > 0) :
         res = requests.get('https://api.kawalcorona.com/indonesia')
         data = res.json()
         
+        ticks = time.asctime( time.localtime(time.time()) )
         
-        print("==============================================================")
-        print("||      Negara     ||   Positif   ||  Sembuh  || Meninggal ||")
-        print("==============================================================")
-        print("||   ", data[0]['name'],"   ||   ", data[0]['positif'],"   ||   ", data[0]['sembuh'],"   ||   ", data[0]['meninggal'], "   ||")
-        print("==============================================================")
+        print(" ")
+        print("-------------------------------------")
+        print("     Info Update Corona Covid-19     ")
+        print("-------------------------------------")
+        print("Waktu saat ini:", ticks)
+        print(" ")   
+        
+        
+        print("Negara           : ", data[0]['name'])
+        print("Jumlah Positif   : ", data[0]['positif'])
+        print("Jumlah Sembuh    : ", data[0]['sembuh'])
+        print("Jumlah Meninggal : ", data[0]['meninggal'])
+        
+        print(" ")
 
         tanya = input("Ulang Perhitungan atau tidak (y/n) : ")
         if (tanya == 'n'):
